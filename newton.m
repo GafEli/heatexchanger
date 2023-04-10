bf82f8d3dafe88c48bff57d9f32f0523d662b21b
GafEli <gafarovaeliza@mail.ru>
2023-04-10 12:39:58 +0300

    Реализация метода Ньютона на примере произвольного уравнения

--- newton.m
+++ newton.m
@@ -1,18 +1,16 @@
 
-function [Xk k] = newton(f,df,tx)
+function [Xk, k] = newton(f,df,tx)
 x0 = tx(1); % нижняя граница интервала
 x1 = tx(2); % верхняя граница интервала
-F = inline(f); % определяем функцию
-DF = inline(df); % определяем производную
 e = 1e-5; % Погрешность
 % начальные приближения
 Xkm = (x0+x1)/2; 
-Yo = F(x0);
+Yo = f(x0);
 k = 0; % счетчик итераций
 while abs(Yo) > e 
-    Xk = Xkm - F(Xkm)/DF(Xkm);
+    Xk = Xkm - f(Xkm)/df(Xkm);
     Xkm = Xk;
-    Yo = F(Xk);
+    Yo = f(Xk);
     k = k + 1;
 end
 
--- test_newton.m
+++ test_newton.m
@@ -1,10 +1,30 @@
+function test_newton()
 clear, clc
- 
+
+x = -3:0.001:3;
 diap = [0, 3]; % диапазон x
-fun = 'x^3 -2*x - 5'; % функция
-dfun = diff(sym(fun)) % производная
-[x0 n] = newton(fun,dfun,diap);
-x00 = fzero(fun, (diap(1)+diap(2))/2); % решение встроенной ф-цией
+[x0, n] = newton(@fun,@dfun,diap);
+x00 = fzero(@fun, (diap(1)+diap(2))/2); % решение встроенной ф-цией
 fprintf('Корень уравнения х = %f, \n', x0);
 fprintf('Количество итераций равно %d. \n',n);
 fprintf('Результат встроенной функции x = %f \n',x00);
+
+plot(x, fun(x), x0, fun(x0), 'o');
+
+end
+
+function y = fun(x)
+
+y = x.^3 -2.*x - 5;
+
+end
+
+function df = dfun(x)
+
+eps = 1e-8;
+dx = abs(x) * eps;
+
+df = (fun(x + dx) - fun(x - dx)) /(2*dx);
+
+
+end
